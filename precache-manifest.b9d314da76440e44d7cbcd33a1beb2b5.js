161d64fdc73e660f76b6241dd599c74582ca393c
unknown <koustav.manna98@gmail.com>
2020-05-12 16:51:01 +0530

    Updates

---
+++ precache-manifest.b9d314da76440e44d7cbcd33a1beb2b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f54cfd2e2f7d303be6cb1aabea1bb449",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "0539f703dee36bff3e42",
+    "url": "/movie/static/css/main.1eb687a4.chunk.css"
+  },
+  {
+    "revision": "671fa7f913c479b297ef",
+    "url": "/movie/static/js/2.a25c8df0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie/static/js/2.a25c8df0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0539f703dee36bff3e42",
+    "url": "/movie/static/js/main.f59407a3.chunk.js"
+  },
+  {
+    "revision": "3870a7f4b24b70579410",
+    "url": "/movie/static/js/runtime-main.502972a9.js"
+  },
+  {
+    "revision": "5633dcdd777db6398af10770b0392ae0",
+    "url": "/movie/static/media/background.5633dcdd.png"
+  },
+  {
+    "revision": "9604d1e879a8a6a49376e15a5cd1dbd1",
+    "url": "/movie/static/media/spinner.9604d1e8.gif"
+  }
+]);
